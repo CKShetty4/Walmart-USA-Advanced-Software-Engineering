47d87f902a518f0ee5f1766871be948f74539a30
CKShetty4 <ckshetty004@gmail.com>
2024-10-03 07:53:12 +0530

    Implement the popMax method

--- PowerOfTwoMaxHeap.java
+++ PowerOfTwoMaxHeap.java
@@ -26,4 +26,46 @@ public class PowerOfTwoMaxHeap {
             parent = (index - 1) / power; 
         }
     }
+
+    public int popMax() {
+        if (heap.isEmpty()) {
+            return Integer.MIN_VALUE; 
+        }
+    
+        int max = heap.get(0); 
+        heap.set(0, heap.get(heap.size() - 1)); 
+        heap.remove(heap.size() - 1); 
+        _siftDown(0); 
+    
+        return max; 
+    }
+    
+    private void _siftDown(int index) {
+        int maxChildIndex = _getMaxChild(index); 
+    
+        while (maxChildIndex != -1 && heap.get(index) < heap.get(maxChildIndex)) {
+            int temp = heap.get(index);
+            heap.set(index, heap.get(maxChildIndex));
+            heap.set(maxChildIndex, temp);
+            index = maxChildIndex;
+            maxChildIndex = _getMaxChild(index); 
+        }
+    }
+    
+    private int _getMaxChild(int index) {
+        int startChild = power * index + 1; 
+        int endChild = Math.min(startChild + power, heap.size()); 
+    
+        int maxChildIndex = -1;
+        int maxChildValue = Integer.MIN_VALUE;
+    
+        for (int i = startChild; i < endChild; i++) {
+            if (heap.get(i) > maxChildValue) {
+                maxChildIndex = i;
+                maxChildValue = heap.get(i);
+            }
+        }
+    
+        return maxChildIndex;
+    }
 }
