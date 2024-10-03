596e52ca0ea478038595e83b36f3fd6d417eb2a9
CKShetty4 <ckshetty004@gmail.com>
2024-10-03 07:50:22 +0530

    project setup and Implement in Java

---
+++ PowerOfTwoMaxHeap.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PowerOfTwoMaxHeap {
+    private int power;
+    private List<Integer> heap;
+
+    public PowerOfTwoMaxHeap(int power) {
+        this.power = power;
+        this.heap = new ArrayList<>();
+    }
+}
