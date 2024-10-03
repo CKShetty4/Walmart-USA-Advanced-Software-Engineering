9f06f8848ffdfa44061521ad20a5f3cf1f8a18fa
CKShetty4 <ckshetty004@gmail.com>
2024-10-03 07:51:28 +0530

    Implement the insert method

--- PowerOfTwoMaxHeap.java
+++ PowerOfTwoMaxHeap.java
@@ -9,4 +9,21 @@ public class PowerOfTwoMaxHeap {
         this.power = power;
         this.heap = new ArrayList<>();
     }
+    public void insert(int value) {
+        heap.add(value); 
+        _siftUp(heap.size() - 1); 
+    }
+    
+    private void _siftUp(int index) {
+        int parent = (index - 1) / power; 
+    
+        
+        while (index > 0 && heap.get(index) > heap.get(parent)) {
+            int temp = heap.get(index);
+            heap.set(index, heap.get(parent));
+            heap.set(parent, temp);
+            index = parent;
+            parent = (index - 1) / power; 
+        }
+    }
 }
