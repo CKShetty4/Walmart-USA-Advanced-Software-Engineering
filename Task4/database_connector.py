b68dff1c64224e092b7b3f057eb73b59795a04eb
CKShetty4 <ckshetty004@gmail.com>
2024-10-03 08:56:51 +0530

    Add functionality to read first spreadsheet, extract and insert shipping data into the database with methods for product and shipment insertion.

--- Task4/database_connector.py
+++ Task4/database_connector.py
@@ -10,3 +10,29 @@ class DatabaseConnector:
 
     def populate(self, spreadsheet_folder):
         """Populate the database with data imported from each spreadsheet."""
+        with open(f"{spreadsheet_folder}/shipping_data_0.csv", "r") as spreadsheet_file_0:
+            csv_reader_0 = csv.reader(spreadsheet_file_0)
+    
+    def populate_first_shipping_data(self, csv_reader_0):
+        """Populate the database with data imported from the first spreadsheet."""
+        for row_index, row in enumerate(csv_reader_0):
+            # ignore the header row
+            if row_index > 0:
+                # extract each required field
+                product_name = row[2]
+                product_quantity = row[4]
+                origin = row[0]
+                destination = row[1]
+
+                # insert the data into the database
+                self.insert_product_if_it_does_not_already_exist(product_name)
+                self.insert_shipment(product_name, product_quantity, origin, destination)
+
+                # give an indication of progress
+                print(f"inserted product {row_index} from shipping_data_0")
+
+    def insert_product_if_it_does_not_already_exist(self, product_name):
+        """Insert a new product into the database. If a product already exists in the database with the given name, ignore it."""
+    
+    def insert_shipment(self, product_name, product_quantity, origin, destination):
+        """Insert a new shipment into the database."""
