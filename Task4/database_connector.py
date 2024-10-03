cd0714582ef127cf2132c6da864429be371e2404
CKShetty4 <ckshetty004@gmail.com>
2024-10-03 09:21:25 +0530

    Implement the populate method

--- Task4/database_connector.py
+++ Task4/database_connector.py
@@ -10,12 +10,18 @@ class DatabaseConnector:
 
     def populate(self, spreadsheet_folder):
         """Populate the database with data imported from each spreadsheet."""
+        # open the spreadsheets
         with open(f"{spreadsheet_folder}/shipping_data_0.csv", "r") as spreadsheet_file_0:
-            csv_reader_0 = csv.reader(spreadsheet_file_0)
-        with open(f"{spreadsheet_folder}/shipping_data_1.csv", "r") as spreadsheet_file_1:
-            csv_reader_1 = csv.reader(spreadsheet_file_1)
-        with open(f"{spreadsheet_folder}/shipping_data_2.csv", "r") as spreadsheet_file_2:
-            csv_reader_2 = csv.reader(spreadsheet_file_2)
+            with open(f"{spreadsheet_folder}/shipping_data_1.csv", "r") as spreadsheet_file_1:
+                with open(f"{spreadsheet_folder}/shipping_data_2.csv", "r") as spreadsheet_file_2:
+                    # prepare the csv readers
+                    csv_reader_0 = csv.reader(spreadsheet_file_0)
+                    csv_reader_1 = csv.reader(spreadsheet_file_1)
+                    csv_reader_2 = csv.reader(spreadsheet_file_2)
+
+                    # populate first spreadsheet
+                    self.populate_first_shipping_data(csv_reader_0)
+                    self.populate_second_shipping_data(csv_reader_1, csv_reader_2)
     
     def populate_first_shipping_data(self, csv_reader_0):
         """Populate the database with data imported from the first spreadsheet."""
